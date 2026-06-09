8472c377036f0fcd173a36dab30585cdb883fc94
Afik17 <afiystudy@gmail.com>
2026-06-09 15:09:18 +0000

    refactor: call rm finalizer inside handle delete

--- internal/controller/handlers/delete.go
+++ internal/controller/handlers/delete.go
@@ -2,30 +2,35 @@ package handlers
 
 import (
 	"context"
-	"fmt"
 
 	githubissuev1alpha1 "github.com/Afik17/GithubIssue/api/v1alpha1"
 	"github.com/Afik17/GithubIssue/internal/controller/core"
+	"github.com/Afik17/GithubIssue/internal/controller/finalizer"
 	"github.com/Afik17/GithubIssue/internal/controller/resources"
 	gh "github.com/Afik17/GithubIssue/internal/github"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 // HandleDelete clean up any resources associated with the issue, such as closing the corresponding GitHub issue if it exists
-func HandleDelete(ctx context.Context, ghClient gh.IGitHub, ghIssue *githubissuev1alpha1.GithubIssue, repoOwner, repoName string) (bool, error) {
+func HandleDelete(ctx context.Context, k8sClient client.Client, ghClient gh.IGitHub, ghIssue *githubissuev1alpha1.GithubIssue, repoOwner, repoName string) error {
 	if !controllerutil.ContainsFinalizer(ghIssue, core.GHIssueDeletionFinalizer) {
-		return false, nil
+		return nil
 	}
 
 	// If the issue was never created, just remove the finalizer on the next step
 	if resources.IsGithubIssueManaged(ctx, ghIssue) {
-		return true, nil
+		return nil
 	}
 
 	err := ghClient.CloseIssue(ctx, repoOwner, repoName, ghIssue.Status.Number)
 	if err != nil {
-		return false, fmt.Errorf("cleanup error, failed to close issue %q: %w", ghIssue.Status.Number, err)
+		return err
 	}
 
-	return true, nil
+	if err := finalizer.Remove(ctx, k8sClient, ghIssue, core.GHIssueDeletionFinalizer); err != nil {
+		return err
+	}
+
+	return nil
 }
